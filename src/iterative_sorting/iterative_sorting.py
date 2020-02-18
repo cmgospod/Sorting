327bb8e703f077c12a84145d006b7097261cb342
cmgospod <cmgospod@users.noreply.github.com>
2020-02-18 15:22:14 -0600

    Iterative sorts no stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,25 @@
-# TO-DO: Complete the selection_sort() function below 
+# TO-DO: Complete the selection_sort() function below
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+        for k in arr[i:]:
+            if k < arr[smallest_index]:
+                smallest_index = arr.index(k)
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    swap_flag = True
+    while swap_flag:
+        swap_flag = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr [i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swap_flag = True
     return arr
 
 
